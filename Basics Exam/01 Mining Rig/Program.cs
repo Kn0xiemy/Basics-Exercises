cf31438af84120e6e55e7c9c09cd5678dbbbb7ee
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2023-05-17 17:23:39 +0300

    uploaded exercises

---
+++ Basics Exam/01 Mining Rig/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01_Mining_Rig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gpuPrice = int.Parse(Console.ReadLine()); // card price
+            int cablePrice = int.Parse(Console.ReadLine());// cable price
+            double usedElectricityPerDay = double.Parse(Console.ReadLine()); // electrict cost for the day
+            double profitPerCardDaily = double.Parse(Console.ReadLine()); // profit at EOD after subtracting the eletricity cost
+                                                                          //Fixed 13 gpus and cables
+            int gpuCost = gpuPrice * 13;
+            int cableCost = cablePrice * 13;
+
+            const int otherComponents = 1000;
+
+            int totalCost = gpuCost + cableCost + otherComponents;
+
+            // 13 cards mining at an example rate of 1,80
+            double profitPerDay = (profitPerCardDaily - usedElectricityPerDay) * 13;
+
+            double daysToRecoverFunds = Math.Ceiling(totalCost / profitPerDay);
+
+            Console.WriteLine(totalCost);
+            Console.WriteLine(daysToRecoverFunds);
+
+        }
+    }
+}
---
+++ Basics Exam/04 Cat Food/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _04_Cat_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int catCount = int.Parse(Console.ReadLine());
+
+            const double kilogramFood = 12.45;
+
+            int groupOne = 0;
+            int groupTwo = 0;
+            int groupThree = 0;
+
+            double totalFoodGrams = 0;
+
+            for (int i = 0; i < catCount; i++)
+            {
+                double gramsOfFood = double.Parse(Console.ReadLine());
+
+                if (gramsOfFood >= 100 && gramsOfFood < 200)
+                {
+                    groupOne++;
+                }
+                else if (gramsOfFood >= 200 && gramsOfFood < 300)
+                {
+                    groupTwo++;
+                }
+                else if (gramsOfFood >=300 && gramsOfFood <= 400)
+                {
+                    groupThree++;
+                }
+
+
+                totalFoodGrams += gramsOfFood;
+
+            }
+
+           
+            double totalPrice = Math.Round(totalFoodGrams / 1000 * kilogramFood, 2);
+
+            Console.WriteLine($"Group 1: {groupOne} cats.");
+            Console.WriteLine($"Group 2: {groupTwo} cats.");
+            Console.WriteLine($"Group 3: {groupThree} cats.");
+            Console.WriteLine($"Price for food per day: {totalPrice:F2} lv.");
+        }
+    }
+}
+
